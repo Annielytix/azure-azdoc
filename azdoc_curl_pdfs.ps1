aa6f87ec20fc78fb60cb3919c0796b1771f7f995
Chris Joakim <christopher.joakim@gmail.com>
2017-06-26 13:45:07 -0400

    Spidering 6/26

--- azdoc_curl_pdfs.ps1
+++ azdoc_curl_pdfs.ps1
@@ -1,5 +1,5 @@
 # Chris Joakim, Microsoft
-# Generated on Sat 2017-06-24
+# Generated on Mon 2017-06-26
 
 echo 'fetching: https://opbuildstorageprod.blob.core.windows.net/output-pdf-files/en-us/Azure.azure-documents/live/active-directory.pdf ...'
 curl https://opbuildstorageprod.blob.core.windows.net/output-pdf-files/en-us/Azure.azure-documents/live/active-directory.pdf -OutFile pdf/azdoc-active-directory.pdf
